a116a603baa89d912d493401a075375e19c3ec3b
Lance Krasniqi <kaizoku-lance@hotmail.com>
2017-12-02 13:49:32 +0100

    drag handler work in progress, added test materials, added draggable tag

---
+++ Assets/Scripts/Controls/DragHandler.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragHandler : MonoBehaviour
+{
+
+    [SerializeField]
+    private Camera m_ShipCamera;
+
+    [SerializeField]
+    private Vector3 m_LeftPoint, m_RightPoint;
+    private Vector3 m_MouseDelta;
+    private bool m_Dragging;
+
+    private void Start()
+    {
+        transform.position = m_LeftPoint;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray _ray = m_ShipCamera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit _hit;
+
+        if (!m_Dragging)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                if (Physics.Raycast(_ray, out _hit))
+                {
+                    if (_hit.collider.CompareTag("Draggable"))
+                    {
+                        m_Dragging = true;
+                        m_MouseDelta = Input.mousePosition;
+                    }
+                }
+            }
+        }
+        else
+        {
+            if (Input.GetMouseButtonUp(0))
+            {
+                m_Dragging = false;
+            }
+            else if (Input.GetMouseButton(0))
+            {
+                UpdateDrag();
+            }
+        }
+    }
+
+    void UpdateDrag()
+    {
+        float _delta = Mathf.Clamp01(Mathf.InverseLerp(m_LeftPoint.x, m_RightPoint.x, Input.mousePosition.x));
+        Debug.Log(_delta);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(m_LeftPoint, m_RightPoint);
+    }
+}
