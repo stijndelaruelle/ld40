17a0e74f56292f90b233de75c37b066eb3e46fff
Stijn Delaruelle <stijndelaruelle@gmail.com>
2017-12-04 23:34:47 +0100

    Small fixes in the game & main menu.

--- Assets/Scripts/UI/DubloonCounter.cs
+++ Assets/Scripts/UI/DubloonCounter.cs
@@ -34,7 +34,7 @@ public class DubloonCounter : MonoBehaviour
         yield return new WaitForSeconds(m_StartDelay);
 
         float currentValue = 0.0f;
-        float targetValue = m_Ship.GetLootValue();
+        float targetValue = m_Ship.GetLootValue() + 100; //Always at least 1 bottle of rum!
         float timer = 0.0f;
 
         while (targetValue > currentValue)
