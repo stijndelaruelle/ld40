cbdd42f73894150c682cddb0e7a9324ecf342478
Lance Krasniqi <kaizoku-lance@hotmail.com>
2017-12-04 20:25:02 +0100

    added two UI sounds

---
+++ Assets/Scripts/UI/ButtonSounds.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class ButtonSounds : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip m_OnHover;
+    [SerializeField]
+    private AudioClip m_OnClick;
+
+    private AudioSource m_Source;
+
+    public void Start()
+    {
+        m_Source = GetComponent<AudioSource>();
+    }
+
+    public void OnHover()
+    {
+        m_Source.PlayOneShot(m_OnHover);
+    }
+
+    public void OnClick()
+    {
+        m_Source.PlayOneShot(m_OnClick);
+    }
+}
