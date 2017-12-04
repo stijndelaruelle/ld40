bca7c3a249d7b151e9f8e55964b3000f5aaf83c7
Stijn Delaruelle <stijndelaruelle@gmail.com>
2017-12-05 00:28:51 +0100

    You can now press escape to go to the main menu.

---
+++ Assets/Scripts/UI/PressEscape.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PressEscape : MonoBehaviour
+{
+    [SerializeField]
+    private ImageFader m_ImageFader;
+
+    [SerializeField]
+    private AudioFader m_AudioFader;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            ToMainMenu();
+        }
+    }
+
+    public void ToMainMenu()
+    {
+        m_ImageFader.FadeIn(OnFadeInComplete);
+        m_AudioFader.FadeOut();
+    }
+
+    private void OnFadeInComplete()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
